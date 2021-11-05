8e7910082523e3b63ec2718a90029e2ac6f2686a
JFelixZuniga <johnfzuniga@gmail.com>
2021-11-05 16:51:56 -0300

    Braking Bad ADD

---
+++ frases-breaking-bad/README.md
@@ -0,0 +1,12 @@
+## Cotizador de Seguros
+
+Puntos tratados en la práctica
+
+- Consulta a API
+- ✨Uso de CSS-in-JS con Emotion✨
+
+[Ver Proyecto](https://jfelixzuniga.github.io/React_Practice/frases-breaking-bad/build/index.html)
+
+<div align="center">
+    <img src="Captura.jpg">
+</div>
---
+++ frases-breaking-bad/src/App.js
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+import { useState, useEffect } from "react";
+import Frase from "./components/Frase";
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.3s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
+
+function App() {
+  // State de frases
+  const [frase, guardarFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  };
+
+  // Cargar una frase
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  return (
+    <Contenedor>
+      <Frase frase={frase} />
+      <Boton onClick={consultarAPI}>Obtener Frase</Boton>
+    </Contenedor>
+  );
+}
+
+export default App;
